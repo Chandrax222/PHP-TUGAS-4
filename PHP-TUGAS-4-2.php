dae86cabb7e00d0e4ebfb52f9a003be0cf2e9249
Chandra Wiratama Putera <141868663+Chandrax222@users.noreply.github.com>
2023-11-22 23:28:17 +0700

    (File) Perhitungan pembagian antara ari dan pintu
    Melakukan pembagian antara $pintu dan $air dan melakukan hasil bagia di antara ke dua hal tersebut dan mengambil kesimpulannya

---
+++ PHP-TUGAS-4-2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 4-2</title>
+</head>
+<body>
+        
+        <?php
+            $pintu = 523;
+            $air = 8891;
+
+            echo ("Jumlah saluran = 523");
+            echo "<br>";
+            echo ("Jumlah terpakai = 8891");
+            echo "<br>";
+            echo ("Rata - rata pemakaian = .....?");
+            echo "<br>";
+            $hasil = $air/$pintu;
+            echo ("Jawaban : $hasil liter/keluarga");
+        ?>
+</body>
+</html>
